a0f09a3295786d37a4bb6f7b3603b4ce99f1d3b1
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-18 00:35:32 +0300

    queye

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const Queue = {};
+    if (this.length === 0) {
+      return {};
+    }
+    let current = this.head;
+    while (current.next) {
+      Queue.value = current.value;
+      Queue.next = current.next;
+    }
+    Queue.value = current.value;
+    Queue.next = current.next;
+    return Queue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let returnHead = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+    return returnHead;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
+
+const queue = new Queue();
+
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7); // returns the top element from queue and deletes it, returns 1
+// returns { value: 3, next: null }
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+// console.log(queue.getUnderlyingList());
